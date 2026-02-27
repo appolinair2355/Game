d63b7d1e93edff7ae5eb8b861dab6ff8749f47ba
appolinair2355 <felicitationsbien990@gmail.com>
2026-02-27 22:12:33 +0100

    Create server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+// server.js
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 10000;
+
+app.use(express.static(path.join(__dirname)));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`🏆 Questions pour un Champion démarré sur le port ${PORT}`);
+    console.log(`📺 Ouvrez http://localhost:${PORT} pour jouer`);
+});
